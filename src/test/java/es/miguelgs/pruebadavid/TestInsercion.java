0881b74158bcde6d5b30fe9eb2140bf8bf74dd5c
Miguel García Da Silva <mikemgds@gmail.com>
2017-09-10 17:23:46 +0200

    Refactorización

--- src/test/java/es/miguelgs/pruebadavid/TestInsercion.java
+++ src/test/java/es/miguelgs/pruebadavid/TestInsercion.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -23,32 +24,45 @@ public class TestInsercion {
 	@Test
 	public void test() throws InterruptedException{
 		
+		Printer.printMessage("INICIO TEST");
+		
 		CountDownLatch c = new CountDownLatch(1);
-		Observable.from(idsDeuda)	
-				  .flatMap(id -> Observable.just(DatosDeDeuda.getDiasAccionDeuda(id))						  				
-						  				.map(datosDeuda -> Estrategia.getAccionesDetalle(id, 
-						  					   											codsEstrategia, 
-						  					   											datosDeuda.getDiaAccion(), 
-						  					   											datosDeuda.getDiaAccionLlamada(), 
-						  					   											datosDeuda.getArquetipo()))
-									   .subscribeOn(Schedulers.io()) // CON ESTE UTILIZA TANTOS THREADS COMO SEA NECESARIO Y CUANDO TERMINAN LOS MANTIENE PARA REUTILIZAR
-									   //.subscribeOn(Schedulers.computation()) // CON ESTE UTILIZA TANTOS THREADS COMO CORES TENGA LA MAQUINA
-						  			   .map(listaAccionesDetalle -> insertarInBBDD(listaAccionesDetalle, "FILTRO", id))
-						  			   .flatMapIterable(x -> x)
-						  			   .map(accionDetalle -> insertarFilaFichero(accionDetalle.getAccion().getCodAccion(), id, "SHEET"))) // 
-				  .subscribe(codAccionInsertada -> {},
-						     t -> {t.printStackTrace(); c.countDown();},
-						     () -> {Printer.printMessage("Terminado el proceso"); c.countDown();});	
+		this.procesarDeudas(idsDeuda) 
+			.subscribe(codAccionInsertada -> {},
+					   t -> {t.printStackTrace(); c.countDown();},
+				       () -> c.countDown());	
 		
 		c.await();
+		
+		Printer.printMessage("FIN TEST");
+	}
+	
+	private Observable<String> procesarDeudas(Collection<BigDecimal> idsDeuda){
+		return Observable.from(idsDeuda)	
+						 .flatMap(id -> this.procesarDeuda(id)
+								 			.subscribeOn(Schedulers.io()) // CON ESTE UTILIZA TANTOS THREADS COMO SEA NECESARIO Y CUANDO TERMINAN LOS MANTIENE PARA REUTILIZAR
+								 			//.subscribeOn(Schedulers.computation()) // CON ESTE UTILIZA TANTOS THREADS COMO CORES TENGA LA MAQUINA
+		  );
+	}
+	
+	private Observable<String> procesarDeuda(BigDecimal idDeuda){
+		return Observable.just(DatosDeDeuda.getDiasAccionDeuda(idDeuda))						  				
+						 .map(datosDeuda -> Estrategia.getAccionesDetalle(idDeuda, 
+				   											codsEstrategia, 
+				   											datosDeuda.getDiaAccion(), 
+				   											datosDeuda.getDiaAccionLlamada(), 
+				   											datosDeuda.getArquetipo()))
+						 .map(listaAccionesDetalle -> insertarInBBDD(listaAccionesDetalle, "FILTRO", idDeuda))
+						 .flatMapIterable(x -> x)
+						 .map(accionDetalle -> insertarFilaFichero(accionDetalle.getAccion().getCodAccion(), idDeuda, "SHEET"));
 	}
 	
 	private String insertarFilaFichero(String codAccion, BigDecimal id, String sheet){
 		Printer.printMessage("INSERCION EN FICHERO: codAccion: %s, id: %f, sheet: %s", codAccion, id, sheet);
 		try {
-			Thread.sleep(10);
+			TimeUnit.MILLISECONDS.sleep(10);
 		} catch (InterruptedException e) {
-			System.out.println(e);
+			throw new RuntimeException(e);
 		}
 		return codAccion;
 	}
@@ -56,9 +70,9 @@ public class TestInsercion {
 	private Collection<AccionDetalle> insertarInBBDD(Collection<AccionDetalle> accionesCalculadas, String filtro, BigDecimal id){
 		Printer.printMessage("INSERCION EN BBDD: accionesCalculadas: %s, filtro: %s, id: %f", accionesCalculadas, filtro, id);
 		try {
-			Thread.sleep(20 * accionesCalculadas.size());
+			TimeUnit.MILLISECONDS.sleep(20 * accionesCalculadas.size());
 		} catch (InterruptedException e) {
-			System.out.println(e);
+			throw new RuntimeException(e);
 		}
 		return accionesCalculadas;
 	}
